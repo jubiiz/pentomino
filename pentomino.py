daee87b1f59fc4d4e54533cc4ca42394ea59f190
jubiiz <jubiiz.audet@gmail.com>
2021-10-22 22:50:02 -0400

    general idea of the warping done, corner selection too (user done), ROI too

--- pentomino.py
+++ pentomino.py
@@ -13,11 +13,50 @@ def pentomino_from_image(source_file):
     cv2.imshow("unprocessed image", img)
     cv2.waitKey()
 
-    # resizes and thresholds the image ; we need an image of size approx 300:500 (arbitrary, may be made better)
+    #thresholds the image ; we need an image of size approx 300:500 (arbitrary, may be made better)
     _, copy = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    copy = cv2.resize(copy, (1000, 700))
+    rows, cols = copy.shape
+    print("number of rows and collumns ; ", rows, cols)
+
+    # this next section records the four corner of the image (from the user)
+    points = []
+
+    def get_pt(event, x, y, flags, params):
+        if event == cv2.EVENT_RBUTTONDBLCLK or event == cv2.EVENT_LBUTTONDBLCLK:
+            print((x, y))
+            points.append([x, y])
+    
+    cv2.namedWindow("gaussian img", cv2.WINDOW_FREERATIO)
+    # getting user to input the corners
+    print("now is time to click corners (cartesian direction : 1, 2, 3, 4)")
+    cv2.setMouseCallback('gaussian img', get_pt)
     cv2.imshow("gaussian img", copy)
     cv2.waitKey()
+
+    # this section warps the image to that the grid is square (sides should be parallel)
+    # taken from https://opencv24-python-tutorials.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html#geometric-transformations
+    # pretty much directly (as much as there's not many ways to get the same thing...)
+    pts1 = np.float32(points[:3])
+    pts2 = np.float32([[cols-100, 100], [100, 100], [100, cols-100]])
+    print(pts1, pts2)
+    M = cv2.getAffineTransform(pts1,pts2)
+    copy = cv2.warpAffine(copy,M,(cols,rows))
+
+    cv2.namedWindow("warped", cv2.WINDOW_FREERATIO)
+    cv2.imshow("warped", copy)
+    cv2.waitKey()
+
+    # then, only the grid part of the image is kept
+    cropped = copy[100:cols-100, 100:cols-100]
+
+    cv2.namedWindow("cropped", cv2.WINDOW_FREERATIO)
+    cv2.imshow("cropped", cropped)
+    cv2.waitKey()
+
+    # calculations as to the pixel size of one square
+    #TODO
+
+
     return(0)
 
 
